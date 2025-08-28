4dc6098566adf1070dd792aad79215a85247cc18
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2025-08-28 09:36:47 +0530

    Project Configs and VS Code IDE support  (#2)

--- src/robot_emulator/base.py
+++ src/robot_emulator/base.py
@@ -4,26 +4,32 @@
 # Motion - send move/ rotate command over mqtt
 # Virtual Robot - base class for robot emulation
 
-import time
 import threading
+import time
+
 import paho.mqtt.client as mqtt
+
 
 class RobotState:
     """Define robot states"""
 
     BEGIN = "BEGIN"
     RUN = "RUN"
-    WAIT ="WAIT"
+    WAIT = "WAIT"
+
 
 class Motion:
     """Handle movement"""
-    def __init__(self,mqtt_client,robot_id):
+
+    def __init__(self, mqtt_client, robot_id):
         self.client = mqtt_client
         self.robot_id = robot_id
 
     def move_distance(self, speed, distance):
         """Move forward a fixed distance"""
-        self.client.publish(f"robots/{self.robot_id}/motion", f"MOVE {speed} {distance}")
+        self.client.publish(
+            f"robots/{self.robot_id}/motion", f"MOVE {speed} {distance}"
+        )
 
     def rotate_degree(self, speed, angle):
         """Rotate by an angle"""
@@ -35,15 +41,18 @@ class Motion:
 
     def move(self, left_speed, right_speed, duration):
         """Move with differential speeds for a duration"""
-        self.client.publish(f"robots/{self.robot_id}/motion", f"DRIVE {left_speed} {right_speed} {duration}")
+        self.client.publish(
+            f"robots/{self.robot_id}/motion",
+            f"DRIVE {left_speed} {right_speed} {duration}",
+        )
+
 
 class VirtualRobot(threading.Thread):
     """Base class for virtual robot emulation"""
-    """
-    Extend this class and implement 'setup()' and 'loop()'
-    """
 
-    def __init__(self, robot_id, x=0, y=0, heading=0, mqtt_config = None):
+    # TODO Extend this class and implement 'setup()' and 'loop()'
+
+    def __init__(self, robot_id, x=0, y=0, heading=0, mqtt_config=None):
         super().__init__()
         self.robot_id = robot_id
         self.x = x
@@ -53,16 +62,15 @@ class VirtualRobot(threading.Thread):
         self.client = mqtt.Client()
         self.motion = Motion(self.client, self.robot_id)
 
-        #configure mqtt
+        # configure mqtt
         if mqtt_config:
             self.client.username_pw_set(
-                mqtt_config.get("username"),
-                mqtt_config.get("password")
+                mqtt_config.get("username"), mqtt_config.get("password")
             )
             self.client.connect(
-                mqtt_config.get("server","127.0.0.1"),
-                mqtt_config.get("port",1883),
-                mqtt_config.get("keepalive",60)
+                mqtt_config.get("server", "127.0.0.1"),
+                mqtt_config.get("port", 1883),
+                mqtt_config.get("keepalive", 60),
             )
 
             def setup(self):
@@ -72,7 +80,9 @@ class VirtualRobot(threading.Thread):
 
             def loop(self):
                 """Main loop, override this method"""
-                print(f"Robot {self.robot_id} running at position ({self.x}, {self.y}) heading {self.heading}")
+                print(
+                    f"Robot {self.robot_id} running at position ({self.x}, {self.y}) heading {self.heading}"
+                )
                 pass
 
             def run(self):
